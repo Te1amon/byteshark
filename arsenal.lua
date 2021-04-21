5f5a9d2b1fd2ea90aad2f6f05249faf0fd8f6118
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-04-21 08:54:29 -0400

    Created 'arsenal.lua' Placeholder

---
+++ arsenal.lua
@@ -0,0 +1,5 @@
+--[[
+Thank you for your patience.
+
+The bugs and etc are currently being worked out. Once we are done, this will be replaced with the obfuscated version of the script (also I'm the only one who works on this hub lol).
+]]

541b1a73ae35045577f7881bb5025c585c12b436
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-04-28 11:29:19 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 286090429 then
+  -- Arsenal
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/byteshark/main/arsenal%20(potato%20pc%20edition)'))();
+  elseif game.PlaceId == 155615604 then
+  -- Prison Life
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/byteshark/main/prison%20life.lua'))();
+  else
+  end

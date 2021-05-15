7e4e22963eacd9f348cc93a47cd224ba9308f14b
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-05-15 14:08:04 -0400

    Added Timmy Wars

--- Main.lua
+++ Main.lua
@@ -9,5 +9,7 @@ if game.PlaceId == 286090429 then
   elseif game.PlaceId == 155615604 then
   -- Prison Life
   loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/byteshark/main/prison%20life.lua'))();
+  elseif game.PlaceId == 5163196050 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/byteshark/main/Timmy%20Wars'))();
   else
   end
